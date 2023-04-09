e526b4bf25d0587c3203ed2990a20153565cbd6f
ding.ning.bj <ding.ning.bj@foxmail.com>
2023-04-10 01:07:03 +0800

    .

--- launch_services.bat
+++ launch_services.bat
@@ -1,17 +1,17 @@
 
 
 pushd .
-cd ..\rest_gpt\
-start launcher.bat
-popd
-
-pushd .
 cd ..\rest_cos\
-start launcher.bat
+rem start launcher.bat
 popd
 
 pushd .
 cd ..\rest_audio\
+start launcher.bat
+popd
+
+pushd .
+cd ..\rest_gpt\
 start launcher.bat
 popd
 
